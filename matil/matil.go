e076314daabf0a80d5fc2c867bc540ccea09b0b2
Fyrna N. <imfyrna@gmail.com>
2025-09-26 03:53:42 +0700

    matil remove clamp

--- matil/matil.go
+++ matil/matil.go
@@ -1,13 +1,14 @@
 package matil
 
+// Number is a type constraint that includes all integer and floating-point types.
 type Number interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
 		~float32 | ~float64
 }
 
-// === math sd ===
-
+// Add returns the sum of all given numbers.
+// If no numbers are provided, it returns the zero value of T.
 func Add[T Number](nums ...T) T {
 	var sum T
 	for _, n := range nums {
@@ -16,6 +17,8 @@ func Add[T Number](nums ...T) T {
 	return sum
 }
 
+// Sub subtracts all subsequent numbers from the first.
+// If no numbers are provided, it returns the zero value of T.
 func Sub[T Number](nums ...T) T {
 	if len(nums) == 0 {
 		var zero T
@@ -26,10 +29,11 @@ func Sub[T Number](nums ...T) T {
 	for _, n := range nums[1:] {
 		res -= n
 	}
-
 	return res
 }
 
+// Mul returns the product of all given numbers.
+// If no numbers are provided, it returns the zero value of T.
 func Mul[T Number](nums ...T) T {
 	if len(nums) == 0 {
 		var zero T
@@ -40,10 +44,12 @@ func Mul[T Number](nums ...T) T {
 	for _, n := range nums {
 		res *= n
 	}
-
 	return res
 }
 
+// Div divides the first number by all subsequent numbers.
+// It panics if division by zero is attempted.
+// If no numbers are provided, it returns the zero value of T.
 func Div[T Number](nums ...T) T {
 	if len(nums) == 0 {
 		var zero T
@@ -53,14 +59,15 @@ func Div[T Number](nums ...T) T {
 	res := nums[0]
 	for _, n := range nums[1:] {
 		if n == 0 {
-			panic("you stupid! division by zero?!")
+			panic("division by zero")
 		}
 		res /= n
 	}
-
 	return res
 }
 
+// Min returns the smallest number among the provided values.
+// If no numbers are provided, it returns the zero value of T.
 func Min[T Number](nums ...T) T {
 	if len(nums) == 0 {
 		var zero T
@@ -73,10 +80,11 @@ func Min[T Number](nums ...T) T {
 			res = n
 		}
 	}
-
 	return res
 }
 
+// Max returns the largest number among the provided values.
+// If no numbers are provided, it returns the zero value of T.
 func Max[T Number](nums ...T) T {
 	if len(nums) == 0 {
 		var zero T
@@ -89,10 +97,11 @@ func Max[T Number](nums ...T) T {
 			res = n
 		}
 	}
-
 	return res
 }
 
+// Avg returns the arithmetic mean of the given numbers.
+// If no numbers are provided, it returns the zero value of T.
 func Avg[T Number](nums ...T) T {
 	if len(nums) == 0 {
 		var zero T
@@ -101,8 +110,7 @@ func Avg[T Number](nums ...T) T {
 	return Add(nums...) / T(len(nums))
 }
 
-// === transform ===
-
+// Abs returns a slice containing the absolute values of the input numbers.
 func Abs[T Number](nums ...T) []T {
 	res := make([]T, len(nums))
 	for i, n := range nums {
@@ -115,6 +123,7 @@ func Abs[T Number](nums ...T) []T {
 	return res
 }
 
+// Neg returns a slice containing the negated values of the input numbers.
 func Neg[T Number](nums ...T) []T {
 	res := make([]T, len(nums))
 	for i, n := range nums {
@@ -123,6 +132,7 @@ func Neg[T Number](nums ...T) []T {
 	return res
 }
 
+// Square returns a slice containing the square of each input number.
 func Square[T Number](nums ...T) []T {
 	res := make([]T, len(nums))
 	for i, n := range nums {
@@ -131,6 +141,7 @@ func Square[T Number](nums ...T) []T {
 	return res
 }
 
+// Cube returns a slice containing the cube of each input number.
 func Cube[T Number](nums ...T) []T {
 	res := make([]T, len(nums))
 	for i, n := range nums {
@@ -139,8 +150,10 @@ func Cube[T Number](nums ...T) []T {
 	return res
 }
 
-// === misc ===
-
+// Mod performs modulo operation sequentially from left to right.
+// Example: Mod(20, 7, 3) => ((20 % 7) % 3).
+// It panics if modulo by zero is attempted.
+// If fewer than 2 numbers are provided, it returns 0.
 func Mod(nums ...int) int {
 	if len(nums) < 2 {
 		return 0
@@ -149,14 +162,15 @@ func Mod(nums ...int) int {
 	res := nums[0]
 	for _, n := range nums[1:] {
 		if n == 0 {
-			panic("mod by zero")
+			panic("modulo by zero")
 		}
 		res %= n
 	}
-
 	return res
 }
 
+// Sign returns a slice with the sign of each input number.
+// The result is 1 for positive numbers, -1 for negative numbers, and 0 for zero.
 func Sign(nums ...float64) []float64 {
 	res := make([]float64, len(nums))
 	for i, n := range nums {
@@ -170,15 +184,4 @@ func Sign(nums ...float64) []float64 {
 		}
 	}
 	return res
-}
-
-// does stdlib go have this tho?
-func Clamp(x, min, max float64) float64 {
-	if x < min {
-		return min
-	}
-	if x > max {
-		return max
-	}
-	return x
 }
